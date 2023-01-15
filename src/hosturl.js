6be50a2e1cfea567c7065c803e4ec774d47a111c
meltphace7 <brd9326@protonmail.com>
2023-01-15 12:25:21 -0800

    changed hosturl

--- src/hosturl.js
+++ src/hosturl.js
@@ -1,3 +1,4 @@
-const hostURL = "http://localhost:8080";
+const hostURL = "https://trail-tracker-backend.cyclic.app";
 // "http://localhost:8080"
+// "https://trail-tracker-backend.cyclic.app"
 export default hostURL;
