65fbd3aec4076d3a00994f286841b8ebd5f900a5
meltphace7 <brd9326@protonmail.com>
2023-01-20 09:31:13 -0800

    cleaned up image slider styles

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import classes from "./ImageSlider.module.css";
-import { IoIosArrowBack } from 'react-icons/io';
-import { IoIosArrowForward } from 'react-icons/io';
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
 
 const ImageSlider = (props) => {
   const [current, setCurrent] = useState(1);
@@ -19,14 +19,12 @@ const ImageSlider = (props) => {
     setCurrent(current === length - 1 ? 0 : current + 1);
   };
 
+  // Hides dots if there are too many to fit on screen
+  // Image count displayed in numbers instead
   const dotsContainerClasses =
     props.images.length > 20
-      ? `${classes["dots-container"]} ${classes["noShow"]}`
+      ? `${classes["dots-container"]} ${classes["no-show"]}`
       : classes["dots-container"];
-    
-
- 
-  
 
   return (
     <div className={classes["slider-container"]}>
@@ -63,13 +61,15 @@ const ImageSlider = (props) => {
             );
           })}
         </div>
-        {props.images.length > 20 && <p>{`${current + 1} / ${props.images.length + 1}`}</p>}
+        {props.images.length > 20 && (
+          <p>{`${current + 1} / ${props.images.length + 1}`}</p>
+        )}
       </div>
       <IoIosArrowForward
         className={classes["slider-button"]}
         onClick={nextHandler}
       />
-      <div className={classes["slider-controls-container"]}>
+      <div className={classes["mobile-slider-controls-container"]}>
         <IoIosArrowBack
           className={classes["mobile-slider-button"]}
           onClick={prevHandler}
